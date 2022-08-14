8b3ef2b1603a1e3e29979cc5d4149b5f4b4a338b
Ubuntu <ubuntu@ip-172-31-41-148.us-west-2.compute.internal>
2022-08-14 21:34:30 +0000

    Adding the MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,9 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY';
+export const STORE_MEMORY = 'STORE_MEMORY';
+export const ADD_MEMORY = 'ADD_MEMORY'
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +12,20 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) => {
+    return({type:CHANGE_OPERATION, payload: operator})
+}
+
+export const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY})
+}
+
+export const storeMemory = ()=> {
+    return({type:STORE_MEMORY})
+}
+
+export const addMemory = ()=>{
+    return{type:ADD_MEMORY}
 }
